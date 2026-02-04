ed44901fc58cac994c9a0d4e9c97ebc1f7393807
AlexandreLeGrand1 <iskandardusbekov1@gmail.com>
2026-02-04 16:26:51 +0500

    Model yaratildi
    -card modeli yasaldi
    -magrationst qilindi

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,38 @@
+"""
+Card
+"""
 from django.db import models
 
 # Create your models here.
+
+class Card(models.Model):
+    """
+    Card model
+
+    Qatorlar:
+
+    --karta raqam
+    --amal qilish muddati
+    --telefon raqam
+    -- statut
+
+    """
+    S_ACTIVE="active"
+    S_INACTIVE="inactive"
+    S_EXPIRED="expired"
+
+    STATUS_CHOICES = [
+        (S_ACTIVE,"Active"),
+        (S_INACTIVE,"Inactive"),
+        (S_EXPIRED,"Expired"),
+    ]
+    card_number = models.CharField(max_length=16, unique=True)
+    expire = models.DateTimeField()
+    phone = models.CharField(max_length=13 ,unique=True)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default=S_ACTIVE
+    )
+    def __str__(self):
+        return str(self.card_number)
